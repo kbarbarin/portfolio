8da866ffd5032e92e0edc679632e5850adc53ae9
Killian Barbarin <killianb31@gmail.com>
2024-06-02 01:24:57 +0200

    ADD bigging of animation in contact

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -1,51 +1,70 @@
-import React from "react";
-import { motion } from 'framer-motion';
+import React, {useEffect} from "react";
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from "react-intersection-observer";
 
 import './Contact.css';
 
 const Contact = () => {
     const questionMarkVariants = {
-        initial: {
-          scale: 1,
-          rotate: 0,
+        hidden: {
+            scale: 1,
+            rotate: 0,
         },
-        animate: {
-          scale: [1, 1.2, 1],
-          rotate: [0, 20, -20, 0],
-          transition: {
-            duration: 2,
-            ease: 'easeInOut',
-            loop: Infinity,
-          },
+        visible: {
+            scale: [1, 3, 2],
+            rotate: 360,
+            transition: {
+                duration: 2,
+                ease: 'easeInOut',
+                loop: Infinity,
+            },
         },
-      };
+    };
+
+    const { ref, inView } = useInView();
+
+    const controls = useAnimation();
+    useEffect(() => {
+        if (inView) {
+            controls.start("visible");
+        }
+        if (!inView) {
+            controls.start("hidden");
+        }
+    }, [inView, controls]);
 
     return (
-        <div className="contact-container">
-      <motion.div 
-        className="question-mark" 
-        variants={questionMarkVariants} 
-        initial="initial" 
-        animate="animate"
-      >
-        <p>?</p>
-      </motion.div>
-      <form className="contact-form">
-        <label>
-          Name:
-          <input type="text" name="name" />
-        </label>
-        <label>
-          Email:
-          <input type="email" name="email" />
-        </label>
-        <label>
-          Message:
-          <textarea name="message" />
-        </label>
-        <button type="submit">Send</button>
-      </form>
-    </div>
+        <React.Fragment>
+            <h1 className="contact-title">Contact</h1>
+
+            <div className="contact-container">
+                <motion.div
+                    className="question-mark"
+                    variants={questionMarkVariants}
+                    initial="hiden"
+                    animate={controls}
+                    ref={ref}
+                >
+                    ?
+                </motion.div>
+                <form className="contact-form">
+                    <label>
+                        Name:
+                        <input type="text" name="name" />
+                    </label>
+                    <label>
+                        Email:
+                        <input type="email" name="email" />
+                    </label>
+                    <label>
+                        Message:
+                        <textarea name="message" />
+                    </label>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
+        </React.Fragment>
+
     );
 }
 
