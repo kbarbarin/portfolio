dee68e55f445c7b74f74ea578330508678a5a918
Killian Barbarin <killianb31@gmail.com>
2025-02-11 15:17:29 +0100

    UPDATE Hoho page with good links

--- src/component/Projects/ProjectsDetails/HoHo/HoHo.jsx
+++ src/component/Projects/ProjectsDetails/HoHo/HoHo.jsx
@@ -4,7 +4,7 @@ import './HoHo.css';
 
 const HoHo = () => {
     const github = 'https://github.com/kbarbarin/secretSanta';
-    const website = "";
+    const website = "https://secretsanta-c60ad.web.app";
 
     return (
       <div className="hoho-project" id="projects">
@@ -14,9 +14,9 @@ const HoHo = () => {
               <h2 className="hoho-project-name">HO-HO</h2>
               <p className="hoho-project-description">Ho-Ho est un site web qui vous permet de créer et de répartir secrètement les participants de votre groupe et propose des idées cadeaux personnalisées pour rendre vos échanges de cadeaux encore plus spéciaux. Faites de la magie des fêtes une réalité en organisant des célébrations festives et pleines de surprises avec notre plateforme conviviale. Faites plaisir à vos proches en offrant des cadeaux qui montrent que vous les connaissez vraiment, le tout dans un esprit de mystère et de joie. Il utilise du JS, React, Node, ...</p>
               <div className="hoho-project-techno">
-                <img className="hoho-project-left-techno-icon" src="/icons/react-native.svg" alt="react native" />
-                <img className="hoho-project-left-techno-icon" src="/icons/google-cloud.svg" alt="google cloud plateform" />
-                <img className="hoho-project-left-techno-icon elastic" src="/icons/elastic.svg" alt="elasticsearch" />
+                <img className="hoho-project-left-techno-icon" src="/icons/react.svg" alt="react" />
+                <img className="hoho-project-left-techno-icon" src="/icons/sass.svg" alt="scss" />
+                <img className="hoho-project-left-techno-icon" src="/icons/nodejs.svg" alt="nodejs" />
                 <img className="hoho-project-left-techno-icon" src="/icons/firebase.svg" alt="firebase" />
               </div>
             </div>
